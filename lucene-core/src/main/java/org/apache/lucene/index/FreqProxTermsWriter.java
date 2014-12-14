94509e5e973bcace3595b7102db228634da87905
yipen9 <yipen9@gmail.com>
2014-12-14 19:54:52 +0800

    增加注释

--- lucene-core/src/main/java/org/apache/lucene/index/FreqProxTermsWriter.java
+++ lucene-core/src/main/java/org/apache/lucene/index/FreqProxTermsWriter.java
@@ -99,7 +99,7 @@ final class FreqProxTermsWriter extends TermsHash {
 
     Fields fields = new FreqProxFields(allFields);
 
-    applyDeletes(state, fields);
+    applyDeletes(state, fields); 
 
     FieldsConsumer consumer = state.segmentInfo.getCodec().postingsFormat().fieldsConsumer(state);
     boolean success = false;
